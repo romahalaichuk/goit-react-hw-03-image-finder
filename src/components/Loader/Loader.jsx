2dafda351bd096fccdaa3479e9106df4906cdb72
romanhalaichuk <vi2virbien@gmail.com>
2024-01-27 23:55:29 +0100

    jfj

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { MutatingDots } from 'react-loader-spinner';
+// import { MutatingDots } from 'react-loader-spinner';
+import Lottie from 'react-lottie';
+import anikiHamsterAnimation from './Aniki Hamster.json';
 
 const Loader = () => {
+  const lottieOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: anikiHamsterAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
   return (
     <div className="loader">
-      <MutatingDots color="#00BFFF" height={150} width={150} />
+      <div style={{ marginTop: 20 }}>
+        <Lottie options={lottieOptions} height={350} width={350} />
+        {/* <MutatingDots color="#00BFFF" height={150} width={150} /> */}
+      </div>
     </div>
   );
 };
